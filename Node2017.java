8dc382a110f91ee06e117a5dcac21bf6b2a51960
Steven R <steve@Mercury>
2017-11-11 15:38:34 -0700

    Add Frame sending functionality (needs working switch to test)

--- Node2017.java
+++ Node2017.java
@@ -8,18 +8,21 @@ import java.util.ArrayList;
 public class Node2017 extends Thread {
 
     ArrayList<String> outdata = new ArrayList<>();
+    public int switchport = 0;
     private int portnum;
     private int nodenum;
-    private Frame a; //to use frame in run, we should declare it here
+    private Frame a; //to use frame in run, we should declare it herem just in case i'll leave this
+    int sleep_duration = 500; //ms --> 1/2 sec
     //private Socket sock;
     
     private PrintWriter pipe;
     File output_file;
 
     //read file, setup frame for all data to be sent (frame: [src][dest][size/ack][data])
-    public Node2017(int portnum, int nodenum) {
+    public Node2017(int portnum, int nodenum, int switch_port) {
         setPortnum(portnum);
         setNodenum(nodenum);
+        switchport = switch_port;
         
         //filename should be of noden for some n
         //directory should be of thisfile'sspot/nodes/output/noden.txt
@@ -51,7 +54,7 @@ public class Node2017 extends Thread {
                 String[] split = s.split(":");
 
                 a = new Frame(nodenum, Integer.parseInt(split[0]), split[1]);
-                //outdata.add(a.toBinaryString());
+                outdata.add(a.toBinaryString());
 
             }
             br.close();
@@ -64,7 +67,43 @@ public class Node2017 extends Thread {
 
     @Override
     public void run() {
+    	
+    	int numofelements = outdata.size(); //# of frames to send
+    	PrintWriter pt;
+    	String out_data;
+    	
+    	while(true) { //infinite loop, until we break
+    		try {
+    			Socket send_out;
+    			
+    			send_out = new Socket(InetAddress.getLocalHost(), switchport);
+    			
+    			pt = new PrintWriter(send_out.getOutputStream(), true);
+    			
+    			for(int k = 0; k < numofelements; k++) {
+    				out_data = outdata.get(k); //get our binary string, that is converted already
+    				System.out.println("Sent: " + out_data);
+    				pt.print(out_data);
+    			}
+    			
+    			pt.close();
+    			send_out.close();
+    			
+    		}catch(Throwable e) {
+    			try {
+    				Thread.sleep(sleep_duration);
+    			}catch(InterruptedException err){
+    				continue;
+    			};
+    		};
+    		break;
+    	}
     	//System.out.println("Hello World!\n");
+    	
+    	//now the fun beings
+    	
+    	
+    	
         /*
         while(!outdata.isEmpty()) {
             System.out.println(outdata.get(0));
