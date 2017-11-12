46ed60721a85c46bbbcabe7c13a9ecc67fe7229a
Tyler Shellberg <tyler.shellberg@student.nmt.edu>
2017-11-12 15:13:36 -0700

    Made node into seperate threads, hopefully

--- Main.java
+++ Main.java
@@ -6,11 +6,9 @@ public class Main {
     public static void main(String[] args) {
         //Get the number of nodes to be instantiated from the command line argument
         int num_nodes = Integer.parseInt(args[0]);
-        //int num_nodes = 25; //hardcoded for now
+
         //Preparation for node management
         int i = 1, serverswitch = 50000;
-
-
 
         //make sure num of nodes stays within defined range.
         if(num_nodes < 2 || num_nodes > 255) {
@@ -29,7 +27,7 @@ public class Main {
 
         //switch server port will be 50000
         System.out.println("it works");
-        //THIS IS CURRENTLY EXPERIMENTAL
+
         //populate the list with node objects to keep organization
         ArrayList<Node2017> nodegroup = new ArrayList<Node2017>();
         i = 1;
@@ -41,21 +39,13 @@ public class Main {
 
         }
 
-
-
-        //I"M TRYING TO WORK WITH THIS
         //Instantiate a number of nodes between 0 and 255 from the command line arguments
         for(int i1 = 1; i1 <= num_nodes; i1++) {
-            //Get the name of the file that node will open
-            /*
-            String fileName = "node" + String.valueOf(i1);
+            //Construct a new Thread for each node
+            Thread nodeThread = new Thread(nodegroup.get(i1));
 
-            //Create the new node
-            Node2017 newNode = new Node2017(50000+i1, i1, serverswitch); */
-            // /*
-
-            //Run it
-            nodegroup.get(i1).run();
+            //Start up the switch (Note, according to pdf it must be a thread of its own)
+            nodeThread.start();
         }
 
         //When all nodes are done, shut them all down
