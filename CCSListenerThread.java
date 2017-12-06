7255cb70cd79365a4fb86c09a5812d5b6bd181ce
eegarcia00 <33458580+eegarcia00@users.noreply.github.com>
2017-12-06 00:32:01 -0700

    Update CCSListenerThread.java

--- CCSListenerThread.java
+++ CCSListenerThread.java
@@ -9,63 +9,63 @@ import java.net.Socket;
 //This listener thread will listen for frames from other switches
 public class CCSListenerThread extends Thread {
 
-	private int port;
-	public Socket socket = null;
-	public ServerSocket listener = null;
-	private CCSSwitch reference;
-	private BufferedReader incoming = null;
-	
-	public CCSListenerThread(int port, CCSSwitch outerSwitch) {
-		this.port = port;
-		this.reference = outerSwitch;
-	}
-	
-	public void run() {
-		int i = 0;	
-		String incomingData = null;
-		
-		msg("Intializing Listener...");
-		try {
-			
-			listener = new ServerSocket(port);
-			
-			while(true) {
-		
-				if(CCSSwitch.Terminate) {
-					return;
-				}
-				msg("Listening...");
-				socket = listener.accept();
-				
-				incoming = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				//msg("Recieved something...");
-				while(true) {
-					incomingData = incoming.readLine();
-					//msg("Incoming: " + incomingData.toString());
+ private int port;
+    public Socket socket = null;
+    public ServerSocket listener = null;
+    private CCSSwitch reference;
+    private BufferedReader incoming = null;
 
-					if(incomingData.equals("TERMINATE")) {
-						break;
-					}
-					
-					Frame fr = new Frame(incomingData); //should arrive as a byte array
-					
-					this.reference.addFrame(fr);
-				}		
-			}
-			
-		}catch (Throwable e) {
-			msgPort("ERROR --> " + e.toString(), this.port);
-			//e.printStackTrace();
-		}
-		
-	}
+    public CCSListenerThread(int port, CCSSwitch outerSwitch) {
+        this.port = port;
+        this.reference = outerSwitch;
+    }
 
-	private static void msg(String input) {
-		System.out.println("CSSListenerThread: "+ input);
-	}
-	
-	private static void msgPort(String input, int port) {
-		System.out.println("CSSListenerThread: Port# " + port + " --> " + input);
-	}
+    public void run() {
+        int i = 0;
+        String incomingData = null;
+
+        msg("Intializing Listener...");
+        try {
+
+            listener = new ServerSocket(port);
+
+            while(true) {
+
+                if(CCSSwitch.Terminate) {
+                    return;
+                }
+                msg("Listening...");
+                socket = listener.accept();
+
+                incoming = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                //msg("Recieved something...");
+                while(true) {
+                    incomingData = incoming.readLine();
+                    //msg("Incoming: " + incomingData.toString());
+
+                    if(incomingData.equals("TERMINATE")) {
+                        break;
+                    }
+
+                    Frame fr = new Frame(incomingData); //should arrive as a byte array
+
+                    this.reference.addFrame(fr);
+                }
+            }
+
+        }catch (Throwable e) {
+            msgPort("ERROR --> " + e.toString(), this.port);
+            //e.printStackTrace();
+        }
+
+    }
+
+    private static void msg(String input) {
+        System.out.println("CSSListenerThread: "+ input);
+    }
+
+    private static void msgPort(String input, int port) {
+        System.out.println("CSSListenerThread: Port# " + port + " --> " + input);
+    }
 	
 }
